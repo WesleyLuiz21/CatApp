459ac9e246ffa45919ff15ad2b7f7e21cb42546e
Wesley Souza <wesley_luiz2001@hotmail.com>
2025-10-25 15:40:52 +0100

    cat commit

--- src/Cat.kt
+++ src/Cat.kt
@@ -3,12 +3,12 @@ class Cat (val name: String, var age: Int, var weight: Int) {
     fun walk() {
         weight--
     }
-
-    override fun toString() : String {
-        return "Name: $name Age: $age"
-    }
-
     fun eat() {
         weight++
     }
+    override fun toString() : String {
+        return "Name: $name Age: $age  weight: $weight"
+    }
+
+
 }
--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,17 @@
 fun main() {
     val binnie = Cat("Binnie", 10, 10)
     val clyde = Cat("Clyde", 5, 5)
+    val Flathead = Cat("Flathead", 7, 10)
+    val Cupra = Cat("Cupra", 2, 7)
     binnie.walk()
     clyde.walk()
     println(binnie)
     println(clyde)
+    Flathead.eat()
+    Flathead.eat()
+    Cupra.walk()
+    Cupra.walk()
+    Cupra.walk()
+    Cupra.walk()
+
 }
